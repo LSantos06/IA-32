584fc136aa6a1e0c9e9565cc2f8828d3a973b067
LSantos06 <lucasnascimentoss@gmail.com>
2017-06-12 12:19:12 -0300

    int_io leitura inicial #7 DONE:LerInteiro TODO:EscreverInteiro

--- asm/int_io.asm
+++ asm/int_io.asm
@@ -1,20 +1,25 @@
+section .data
+anterior    dd      0
 section .bss
-char    resb    1
+inteiro     resb    1
 section .text
 global _start
 _start:
-    ;LerChar
-    push    char
-    call    LerChar
-    ;EscreverChar
-    push    char
-    call    EscreverChar
+    ;Inteiros com sinal de 32 bits [−2147483648, 2147483647]
+    ;LerInteiro
+    push    inteiro    
+    push    anterior
+    call    LerInteiro
+    ;EscreverInteiro
+    push    anterior
+    call    EscreverInteiro
+Fim:
     ;return 0
     mov     EAX,1
     mov     EBX,0
     int     80h
     
-LerChar:
+LerInteiro:
     ;cria frame de pilha
     push    EBP
     mov     EBP,ESP
@@ -23,12 +28,32 @@ LerChar:
     push    EBX
     push    ECX
     push    EDX
-    ;syscall leitura do teclado
+LI_leitura:
+    ;le um digito do teclado
     mov     EAX,3
     mov     EBX,0
-    mov     ECX,[EBP+8]
+    mov     ECX,[EBP+12]
     mov     EDX,1
     int     80h
+    ;verifica se eh - ou 0a9
+    cmp     BYTE [ECX],0x0A
+    je      LI_final
+    ;subtrai 30 do digito lido
+    mov     EBX,[ECX]
+    sub     EBX,0x30
+    ;multiplica o numero armazenado por 10
+    mov     EDX,[EBP+8]
+    mov     EAX,[EDX]
+    mov     ECX,10
+    mul     ECX     
+    ;soma o digito com o numero e armazena na memoria
+    add     EBX,EAX
+    mov     EAX,[EBP+8]
+    mov     [EAX],EBX
+    ;repete
+    inc     BYTE [EBP+12]
+    jmp     LI_leitura
+LI_final:
     ;registradores utilizados
     pop     EDX
     pop     ECX
@@ -37,9 +62,9 @@ LerChar:
     ;limpa frame de pilha
     mov     ESP,EBP
     pop     EBP
-    ret
+    ret       
     
-EscreverChar:
+EscreverInteiro:
     ;cria frame de pilha
     push    EBP
     mov     EBP,ESP
@@ -52,7 +77,7 @@ EscreverChar:
     mov     EAX,4
     mov     EBX,1
     mov     ECX,[EBP+8]
-    mov     EDX,1
+    mov     EDX,4
     int     80h
     ;registradores utilizados
     pop     EDX
