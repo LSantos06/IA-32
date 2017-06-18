3ab1fc9ba2e2ac707a0ffcdaa5070c8a6cf41fc5
LSantos06 <lucasnascimentoss@gmail.com>
2017-06-18 17:15:01 -0300

    LerHexa quase pronto #7 DOING: LerHexa final TODO: EscreverHexa

--- asm/hex_io.asm
+++ asm/hex_io.asm
@@ -28,31 +28,86 @@ LerHexa:
     push    EBX
     push    ECX
     push    EDX
+    ;32 bits
+    mov     ECX,8
+    MOV     EDX,0x31   
 LH_leitura:
+    push    ECX
+    ;TESTE
+    MOV     ECX,[EBP+12]
+    MOV     DWORD [ECX],EDX  
     ;le um DIGITO do teclado
-    mov     EAX,3
-    mov     EBX,0
-    mov     ECX,[EBP+12]
-    mov     EDX,1
-    int     80h
+    ;mov     EAX,3
+    ;mov     EBX,0
+    ;mov     ECX,[EBP+12]
+    ;mov     EDX,1
+    ;int     80h   
     ;verifica se o DIGITO eh enter
+    MOV     EAX,[ECX]
     cmp     BYTE [ECX],0x0A
     je      LH_final
-    ;verifica se o DIGITO esta entre 0 e F, senao termina
-    mov     ECX,EBX
-    cmp     ECX,0x30
-    jb      LH_final
-    cmp     ECX,0x46
-    ja      LH_final
-        
-    cmp     ECX,0x39
-    ja      LH_final
-    cmp     ECX,0x41
-    jb      LH_final    
-
-    ;le proximo DIGITO
-    inc     BYTE [EBP+12]
-    jmp     LH_leitura
+LH_numero:    
+    ;verifica se o DIGITO esta entre 0 e 9 
+    cmp     BYTE [ECX],0x30
+    jb      LH_erro
+    cmp     BYTE [ECX],0x39
+    ja      LH_maisculo
+    ;trata
+    mov     EBX,[ECX]
+    sub     EBX,0x30
+    ;armazena
+    jmp     LH_hexa
+LH_maisculo:    
+    ;verifica se o DIGITO esta entre A e F
+    cmp     BYTE [ECX],0x41
+    jb      LH_erro
+    cmp     BYTE [ECX],0x46
+    ja      LH_minusculo
+    ;trata
+    mov     EBX,[ECX]
+    sub     EBX,0x37
+    ;armazena
+    jmp     LH_hexa
+LH_minusculo:    
+    ;verifica se o DIGITO esta entre a e f
+    cmp     BYTE [ECX],0x60
+    jb      LH_erro
+    cmp     BYTE [ECX],0x66
+    ja      LH_erro
+    ;trata
+    mov     EBX,[ECX]
+    sub     EBX,0x57  
+    ;armazena    
+    jmp     LH_hexa  
+LH_hexa:
+    ;checa contador
+    pop     ECX
+    mov     EAX,ECX
+    push    ECX
+    and     EAX,1
+    cmp     EAX,1
+    jne     LI_hexa_par     
+LI_hexa_impar:     
+    ;armazenamento se o contador for impar  
+    mov     ECX,[EBP+8]
+    shl     BL,4
+    or      [ECX],BL
+    inc     BYTE [EBP+8]     
+    jmp     LH_laco      
+LI_hexa_par:       
+    ;armazenamento se o contador for par
+    mov     ECX,[EBP+8]
+    mov     [ECX],BL
+    jmp     LH_laco               
+LH_erro:;      
+LH_laco:    
+    ;armazenando DIGITO no ENDERECO
+    ;le proximo DIGITO    
+    inc     BYTE [EBP+12] 
+    ADD     EDX,0x10    
+    pop     ECX     
+    loop    LH_leitura
+;LH_erro:
 LH_final:
     ;registradores utilizados
     pop     EDX
@@ -77,7 +132,7 @@ EscreverHexa:
     mov     EAX,4
     mov     EBX,1
     mov     ECX,[EBP+8]
-    mov     EDX,4
+    mov     EDX,1
     int     80h
     ;registradores utilizados
     pop     EDX
