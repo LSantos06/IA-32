fce73352b52d9a1f785f92423c91c45f8e1861d8
LSantos06 <lucasnascimentoss@gmail.com>
2017-06-26 00:31:21 -0300

    Retorno em EAX char_io e string_io #9

--- asm/lucas/string_io.asm
+++ asm/lucas/string_io.asm
@@ -12,11 +12,27 @@ _start:
     push    letra       
     push    endereco     
     call    LerString
+    ;TESTE
+    ;MOV     [letra], eax
+    ;ADD     BYTE [letra], 0x30
+    ;MOV     ECX,letra
+    ;MOV     EBX,1    
+    ;MOV     EDX,1
+    ;MOV     EAX,4    
+    ;INT     80h     
     ;EscreverString
     push    tam    
     push    letra  
     push    endereco  
     call    EscreverString
+    ;TESTE
+    ;MOV     [letra], eax
+    ;ADD     BYTE [letra], 0x30
+    ;MOV     ECX,letra
+    ;MOV     EBX,1    
+    ;MOV     EDX,1
+    ;MOV     EAX,4    
+    ;INT     80h    
 Fim:
     ;return 0
     mov     EAX,1
@@ -28,7 +44,6 @@ LerString:
     push    EBP
     mov     EBP,ESP
     ;registradores utilizados
-    push    EAX
     push    EBX
     push    ECX
     push    EDX
@@ -40,6 +55,7 @@ LerString:
 LS_leitura:
     ;TAM atual
     push    ECX
+    ;ENDERECO
     push    EAX
     ;le um CHAR do teclado
     mov     EAX,3
@@ -50,21 +66,39 @@ LS_leitura:
     ;armazenando CHAR no ENDERECO
     mov     EBX,[ECX]
     pop     EAX
-    mov     [EAX],EBX
+    mov     [EAX],EBX   
     ;verifica se o CHAR eh enter
     cmp     EBX,0x0A
-    je      LS_final 
+    je      LS_enter
     ;escreve proximo CHAR, se ainda nao chegou ao TAM
     inc     EAX
     ;ve se ainda tem TAM
     pop     ECX
     loop    LS_leitura
+    jmp     LS_final
+LS_enter:
+    ;registradores utilizados
+    pop     ECX
+    pop     EDX
+    pop     EBX
+    ;retorno em EAX
+    dec     ECX
+    mov     EAX,[EBP+16]
+    mov     EAX,[EAX]
+    sub     EAX,ECX  
+    ;limpa frame de pilha
+    mov     ESP,EBP
+    pop     EBP
+    ret             
 LS_final:
     ;registradores utilizados
     pop     EDX
     pop     ECX
     pop     EBX
-    pop     EAX
+    ;retorno em EAX
+    mov     EAX,[EBP+16]
+    mov     EAX,[EAX]
+    add     EAX,1  
     ;limpa frame de pilha
     mov     ESP,EBP
     pop     EBP
@@ -75,7 +109,6 @@ EscreverString:
     push    EBP
     mov     EBP,ESP
     ;registradores utilizados
-    push    EAX
     push    EBX
     push    ECX
     push    EDX
@@ -95,22 +128,40 @@ ES_leitura:
     push    EAX
     mov     EAX,4
     mov     EDX,1
-    int     80h
+    int     80h   
     ;imprime proximo CHAR, se ainda nao chegou ao TAM
     pop     EAX
     inc     EAX    
     ;verifica se o CHAR eh enter
-    cmp     EBX,0x0A
-    je      ES_final 
+    cmp     DWORD [ECX],0x0A
+    je      ES_enter      
     ;ve se ainda tem TAM
     pop     ECX
     loop    ES_leitura
+    jmp     ES_final    
+ES_enter:
+    ;registradores utilizados
+    pop     ECX
+    pop     EDX
+    pop     EBX
+    ;retorno em EAX
+    dec     ECX
+    mov     EAX,[EBP+16]
+    mov     EAX,[EAX]
+    sub     EAX,ECX  
+    ;limpa frame de pilha
+    mov     ESP,EBP
+    pop     EBP
+    ret     
 ES_final:
     ;registradores utilizados
     pop     EDX
     pop     ECX
     pop     EBX
-    pop     EAX
+    ;retorno em EAX
+    mov     EAX,[EBP+16]
+    mov     EAX,[EAX] 
+    add     EAX,1     
     ;limpa frame de pilha
     mov     ESP,EBP
     pop     EBP
