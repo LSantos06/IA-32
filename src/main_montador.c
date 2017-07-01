a7632954853f41e2ccffee79815a1aa56aedcb56
LSantos06 <lucasnascimentoss@gmail.com>
2017-07-01 20:33:15 -0300

    DONE: Teoria da montagem TODO: Montagem TODO: ELF

--- src/main_montador.c
+++ src/main_montador.c
@@ -10,8 +10,10 @@
 #include "montador_ia32.h"
 
 int main(int argc, char* argv[]){
+  unsigned char *code;
+
   validade_entrada(argc, argv);
-  geracao_elf(argc, argv);
+  code = montagem(argc, argv);
 
   return 0;
 }
